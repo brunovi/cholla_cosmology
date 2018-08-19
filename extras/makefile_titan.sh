3a1c33dae2ad44c76876e76afa0e66a87b3a1964
bruno <bvillasen@gmail.com>
2018-08-19 14:03:42 -0700

    added makefile for Titan

--- extras/makefile_titan.sh
+++ extras/makefile_titan.sh
@@ -9,9 +9,6 @@ CUDAFILES = $(wildcard $(DIR)/*.cu)
 
 
 OBJS   = $(subst .c,.o,$(CFILES)) $(subst .cpp,.o,$(CPPFILES)) $(subst .cu,.o,$(CUDAFILES))
-COBJS   = $(subst .c,.o,$(CFILES))
-CPPOBJS   = $(subst .cpp,.o,$(CPPFILES))
-CUOBJS   = $(subst .cu,.o,$(CUDAFILES))
 
 #To use GPUs, CUDA must be turned on here
 #Optional error checking can also be enabled
@@ -22,8 +19,8 @@ CUDA = -DCUDA #-DCUDA_ERROR_CHECK
 MPI_FLAGS =  -DMPI_CHOLLA
 
 ifdef MPI_FLAGS
-  CC	= mpicc
-  CXX   = mpicxx
+  CC	= cc
+  CXX   = CC
 
   #MPI_FLAGS += -DSLAB
   MPI_FLAGS += -DBLOCK
@@ -46,8 +43,8 @@ OUTPUT = -DHDF5
 
 #RECONSTRUCTION = -DPCM
 #RECONSTRUCTION = -DPLMP
-RECONSTRUCTION = -DPLMC
-#RECONSTRUCTION = -DPPMP
+#RECONSTRUCTION = -DPLMC
+RECONSTRUCTION = -DPPMP
 #RECONSTRUCTION = -DPPMC
 
 #SOLVER = -DEXACT
@@ -57,7 +54,40 @@ SOLVER = -DHLLC
 #INTEGRATOR = -DCTU
 INTEGRATOR = -DVL
 
-COOLING = #-DCOOLING_GPU -DCLOUDY_COOL
+H_CORRECTION = -DH_CORRECTION
+#COOLING = -DCOOLING_GPU
+
+
+DUAL_ENERGY = -DDE
+
+
+# #Turn on SELF_GRAVITY
+# SELF_GRAVITY = -DSELF_GRAVITY
+#
+# ifdef SELF_GRAVITY
+# PFFTINC = /ccs/home/bvilasen/apps/PFFT/pfft-git/include
+# PFFTLIB = /ccs/home/bvilasen/apps/PFFT/pfft-git/lib
+# FFTWINC = /opt/cray/fftw/3.3.4.11/interlagos/include
+# FFTWLIB = /opt/cray/fftw/3.3.4.11/interlagos/lib
+# PFFT_INC = -I$(PFFTINC) -I$(FFTWINC)
+# PFFT_LIBS = -L$(PFFTLIB) -L$(FFTWLIB)
+# PFFTFLAGS = $(PFFT_INC) -openmp -lpfft -lfftw3_mpi -lfftw3
+# endif
+#
+# PFFT_OMP = -DPFFT_OMP
+# ifdef PFFT_OMP
+# PFFTFLAGS += -lpfft_omp
+# endif
+#
+# # Turn on particles
+# PARTICLES = -DPARTICLES
+# LONG_IDS = -DLONG_IDS
+#
+# # Turn on cosmology
+# COSMOLOGY = -DCOSMOLOGY
+# COSMO_UNITS = -DCOSMO_UNITS
+#
+# OMP_OVERLAP_HYDRO = -DOMP_OVERLAP_HYDRO
 
 
 ifdef CUDA
@@ -66,40 +96,40 @@ CUDA_LIBS = -L$(CUDATOOLKIT_HOME)/lib64/ -lcuda -lcudart
 endif
 
 ifeq ($(OUTPUT),-DHDF5)
-HDF5_INCL = -I$(HDF5_DIR)/include
+HDF5_INCLUDE = -I$(HDF5_DIR)/include
 HDF5_LIBS = -L$(HDF5_DIR)/lib -lhdf5
 endif
 
-INCL   = -I./ $(HDF5_INCL)
-NVINCL = $(INCL) $(CUDA_INCL)
-LIBS   = -lm $(HDF5_LIBS) $(CUDA_LIBS)
+INCL   = -I./ $(HDF5_INCLUDE)
+NVINCL = $(INCL) $(CUDA_INCLUDE)
+NVLIBS = $(CUDA_LIBS)
+LIBS   = -lm $(HDF5_LIBS)
 
 
-FLAGS = $(CUDA) $(PRECISION) $(OUTPUT) $(RECONSTRUCTION) $(SOLVER) $(INTEGRATOR) $(COOLING) #-DSTATIC_GRAV #-DDE -DSCALAR -DSLICES -DPROJECTION -DROTATED_PROJECTION
-CFLAGS 	  = $(OPTIMIZE) $(FLAGS) $(MPI_FLAGS)
-CXXFLAGS  = $(OPTIMIZE) $(FLAGS) $(MPI_FLAGS)
-NVCCFLAGS = $(FLAGS) -m64 -fmad=false -ccbin=$(CC) -arch=compute_35
+
+FLAGS = $(CUDA) $(PRECISION) $(OUTPUT) $(RECONSTRUCTION) $(SOLVER) $(INTEGRATOR) $(H_CORRECTION) $(COOLING) $(DUAL_ENERGY) $(SELF_GRAVITY) $(PARTICLES) $(LONG_IDS) $(COSMOLOGY) $(COSMO_UNITS) $(OMP_OVERLAP_HYDRO) $(PFFT_OMP)
+CFLAGS 	  = $(OPTIMIZE) $(FLAGS) $(MPI_FLAGS) $(FFT_FLAGS) -m64
+CXXFLAGS  = $(OPTIMIZE) $(FLAGS) $(MPI_FLAGS) $(FFT_FLAGS) $(PFFTFLAGS) -m64
+NVCCFLAGS = $(FLAGS) -m64 -arch=compute_35 -fmad=false -ccbin=$(CC)
+LDFLAGS	  = -m64
 
 
 %.o:	%.c
 		$(CC) $(CFLAGS)  $(INCL)  -c $< -o $@
 
 %.o:	%.cpp
-		$(CXX) $(CXXFLAGS)  $(INCL) -c $< -o $@
+		$(CXX) $(CXXFLAGS)  $(INCL)  -c $< -o $@
 
 %.o:	%.cu
-		$(NVCC) $(NVCCFLAGS) --device-c $(NVINCL)  -c $< -o $@
-
-src/gpuCode.o:	$(CUOBJS)
-$(NVCC) -arch=compute_35 -dlink $(CUOBJS) -o src/gpuCode.o
-
-$(EXEC): $(OBJS) src/gpuCode.o
-	 	 $(CXX) $(OBJS) src/gpuCode.o $(LIBS) -o $(EXEC)
+		$(NVCC) $(NVCCFLAGS)  $(INCL)  -c $< -o $@
 
 
+$(EXEC): $(OBJS)
+	 	 $(CXX) $(CXXFLAGS) $(OBJS) $(LDFLAGS) $(LIBS) $(NVLIBS) $(PFFTFLAGS) $(PFFT_LIBS) -o $(EXEC) $(INCL)
 
+#$(OBJS): $(INCL)
 
 .PHONY : clean
 
 clean:
-	 rm -f $(OBJS) src/gpuCode.o $(EXEC)
+	 rm -f $(OBJS) $(EXEC)
