084ce4ef10d48f3773a6cd629dd55481fad824f1
bruno <bvillasen@gmail.com>
2018-09-14 12:41:30 -0700

    added wait and unload seondary particles buffers

--- src/mpi_boundaries.cpp
+++ src/mpi_boundaries.cpp
@@ -1617,7 +1617,7 @@ void Grid3D::Unload_Particles_From_Buffers_BLOCK(int index){
 }
 
 void Grid3D::Set_Particles_Secondary_Transfer_Recv( int index, int buffer_start ){
-
+  MPI_Status status;
   Real *recv_buffer;
 
   if ( index == 0 ) recv_buffer = recv_buffer_x0;
@@ -1636,36 +1636,48 @@ void Grid3D::Set_Particles_Secondary_Transfer_Recv( int index, int buffer_start 
     std::cout << "  N Secondary Recv X0: " << n_secondary_transfer << std::endl;
     Check_and_Grow_Particles_Buffer( &recv_buffer_x0_second_particles, &buffer_length_second_particles_x0_recv, buffer_length_secondary );
     MPI_Irecv(recv_buffer_x0_second_particles, buffer_length_secondary, MPI_CHREAL, source[0], 6, world, &recv_request_secondary_particles[0]);
+    MPI_Wait( &recv_request_secondary_particles[0], &status );
+    Unload_Particles_from_Buffer_X_0( true );
     return;
   }
   if ( index == 1 ){
     std::cout << "  N Secondary Recv X1: " << n_secondary_transfer << std::endl;
     Check_and_Grow_Particles_Buffer( &recv_buffer_x1_second_particles, &buffer_length_second_particles_x1_recv, buffer_length_secondary );
     MPI_Irecv(recv_buffer_x1_second_particles, buffer_length_secondary, MPI_CHREAL, source[1], 7, world, &recv_request_secondary_particles[1]);
+    MPI_Wait( &recv_request_secondary_particles[1], &status );
+    Unload_Particles_from_Buffer_X_1( true );
     return;
   }
   if ( index == 2 ){
     std::cout << "  N Secondary Recv Y0: " << n_secondary_transfer << std::endl;
     Check_and_Grow_Particles_Buffer( &recv_buffer_y0_second_particles, &buffer_length_second_particles_y0_recv, buffer_length_secondary );
     MPI_Irecv(recv_buffer_y0_second_particles, buffer_length_secondary, MPI_CHREAL, source[2], 8, world, &recv_request_secondary_particles[0]);
+    MPI_Wait( &recv_request_secondary_particles[0], &status );
+    Unload_Particles_from_Buffer_Y_0( true );
     return;
   }
   if ( index == 3 ){
     std::cout << "  N Secondary Recv Y1: " << n_secondary_transfer << std::endl;
     Check_and_Grow_Particles_Buffer( &recv_buffer_y1_second_particles, &buffer_length_second_particles_y1_recv, buffer_length_secondary );
     MPI_Irecv(recv_buffer_y1_second_particles, buffer_length_secondary, MPI_CHREAL, source[3], 9, world, &recv_request_secondary_particles[1]);
+    MPI_Wait( &recv_request_secondary_particles[1], &status );
+    Unload_Particles_from_Buffer_Y_1( true );
     return;
   }
   if ( index == 4 ){
     std::cout << "  N Secondary Recv Z0: " << n_secondary_transfer << std::endl;
     Check_and_Grow_Particles_Buffer( &recv_buffer_z0_second_particles, &buffer_length_second_particles_z0_recv, buffer_length_secondary );
     MPI_Irecv(recv_buffer_z0_second_particles, buffer_length_secondary, MPI_CHREAL, source[4], 10, world, &recv_request_secondary_particles[0]);
+    MPI_Wait( &recv_request_secondary_particles[0], &status );
+    Unload_Particles_from_Buffer_Z_0( true );
     return;
   }
   if ( index == 5 ){
    std::cout << "  N Secondary Recv Z1: " << n_secondary_transfer << std::endl;
    Check_and_Grow_Particles_Buffer( &recv_buffer_z1_second_particles, &buffer_length_second_particles_z1_recv, buffer_length_secondary );
    MPI_Irecv(recv_buffer_z1_second_particles, buffer_length_secondary, MPI_CHREAL, source[5], 11, world, &recv_request_secondary_particles[1]);
+   MPI_Wait( &recv_request_secondary_particles[1], &status );
+   Unload_Particles_from_Buffer_Z_1( true );
    return;
  }
 }
