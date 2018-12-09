2b21b575cec16a2781fbd3c4407282f490f498eb
bruno <bvillasen@gmail.com>
2018-12-09 13:17:53 -0800

    print transfer status for problematic particle

--- src/particles/particles_boundaries.cpp
+++ src/particles/particles_boundaries.cpp
@@ -448,6 +448,15 @@ void Particles_3D::Unload_Particles_from_Buffer( int direction, int side, int bu
         continue;
       }
       if ( pPos_y >= G.yMax ){
+        if ( pId == 65482143 ){
+          std::cout <<  "pID: " << pId << " Seond Transfer sending UP " << direction << " " << side << std::endl;
+          std::cout << " posX: " << pPos_x << " velX: " << pVel_x << std::endl;
+          std::cout << " posY: " << pPos_y << " velY: " << pVel_y << std::endl;
+          std::cout << " posZ: " << pPos_z << " velZ: " << pVel_z << std::endl;
+          std::cout << " Domain X: " << G.xMin << "  " << G.xMax << std::endl;
+          std::cout << " Domain Y: " << G.yMin << "  " << G.yMax << std::endl;
+          std::cout << " Domain Z: " << G.zMin << "  " << G.zMax << std::endl;
+        }
         // std::cout << "  Adding to Y1 " << std::endl;
         Add_Particle_To_Buffer( send_buffer_y1, buffer_start_y, max_particles, send_buffer_y1_second, 0, pId, pMass, pPos_x, pPos_y, pPos_z, pVel_x, pVel_y, pVel_z, secondary);
         continue;
