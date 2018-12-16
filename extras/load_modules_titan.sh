4cedea9157fe84b2a3d7f1250ef6b47765584e15
bruno <bvillasen@gmail.com>
2018-12-15 18:15:28 -0600

    added test files

--- extras/load_modules_titan.sh
+++ extras/load_modules_titan.sh
@@ -6,3 +6,11 @@ module load python
 module load python_ipython/3.0.0
 module load python_h5py
 module load python_matplotlib
+
+
+module switch PrgEnv-pgi/5.2.82 PrgEnv-gnu
+module use /ccs/proj/ast125/sw/modulefiles
+module load cray-hdf5
+module load python
+module load python_numpy/1.7.1
+module load python_h5py
