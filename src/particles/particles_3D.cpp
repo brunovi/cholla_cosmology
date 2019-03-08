8f605618728727ef396c7ef3cdc8a552c28c3486
bruno <bvillasen@gmail.com>
2019-03-08 10:16:24 -0800

    not printing the omp_domain at initialization of particles

--- src/particles/particles_3D.cpp
+++ src/particles/particles_3D.cpp
@@ -120,22 +120,23 @@ void Particles_3D::Initialize( struct parameters P, Grav3D &Grav, Real xblocal, 
   int n_omp_max = omp_get_max_threads();
   chprintf("  MAX OMP Threads: %d\n", n_omp_max);
   chprintf("  N OMP Threads per MPI process: %d\n", N_OMP_THREADS);
-  // omp_set_num_threads(8);
-  #pragma omp parallel num_threads( N_OMP_THREADS )
-  {
-    int omp_id, n_omp_procs;
-    part_int_t omp_pIndx_start, omp_pIndx_end;
-    int omp_gridIndx_start, omp_gridIndx_end;
 
-    omp_id = omp_get_thread_num();
-    n_omp_procs = omp_get_num_threads();
-    #pragma omp barrier
-    Get_OMP_Indxs( n_local, n_omp_procs, omp_id, G.nz_local + 2*G.n_ghost_particles_grid, &omp_pIndx_start, &omp_pIndx_end, &omp_gridIndx_start, &omp_gridIndx_end );
-
-    for (int omp_indx = 0; omp_indx<n_omp_procs; omp_indx++){
-      if (omp_id == omp_indx) chprintf( "  omp_id:%d  p_start:%ld  p_end:%ld  g_start:%d  g_end:%d\n", omp_id, omp_pIndx_start, omp_pIndx_end, omp_gridIndx_start, omp_gridIndx_end );
-    }
-  }
+  // // Print omp domain for each omp thread
+  // #pragma omp parallel num_threads( N_OMP_THREADS )
+  // {
+  //   int omp_id, n_omp_procs;
+  //   part_int_t omp_pIndx_start, omp_pIndx_end;
+  //   int omp_gridIndx_start, omp_gridIndx_end;
+  //
+  //   omp_id = omp_get_thread_num();
+  //   n_omp_procs = omp_get_num_threads();
+  //   #pragma omp barrier
+  //   Get_OMP_Indxs( n_local, n_omp_procs, omp_id, G.nz_local + 2*G.n_ghost_particles_grid, &omp_pIndx_start, &omp_pIndx_end, &omp_gridIndx_start, &omp_gridIndx_end );
+  //
+  //   for (int omp_indx = 0; omp_indx<n_omp_procs; omp_indx++){
+  //     if (omp_id == omp_indx) chprintf( "  omp_id:%d  p_start:%ld  p_end:%ld  g_start:%d  g_end:%d\n", omp_id, omp_pIndx_start, omp_pIndx_end, omp_gridIndx_start, omp_gridIndx_end );
+  //   }
+  // }
   #endif
 
   #ifdef MPI_CHOLLA
