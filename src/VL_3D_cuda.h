4de15d3ac895a044368cfb3aae8abb2049a27454
bruno <bvillasen@gmail.com>
2019-03-10 14:19:45 -0700

    added SINGLE_ALLOC_GPU for allocating memmory only at the first hydro timestep, only implemented in VL_3D

--- src/VL_3D_cuda.h
+++ src/VL_3D_cuda.h
@@ -10,6 +10,7 @@
 
 Real VL_Algorithm_3D_CUDA(Real *host_conserved0, Real *host_conserved1, int nx, int ny, int nz, int x_off, int y_off, int z_off, int n_ghost, Real dx, Real dy, Real dz, Real xbound, Real ybound, Real zbound, Real dt, int n_fields, Real dens_floor, Real temp_floor );
 
+void Free_Memory_VL_3D();
 
 #endif //VL_3D_CUDA_H
 #endif //CUDA
