58633d369acab571cc3aa90090ae96f15042881f
bruno <bvillasen@gmail.com>
2019-03-11 16:23:37 -0700

    added flag compiler N_DEVICE_PER_NODE to set the number of cuda devices p[er node

--- src/cuda_mpi_routines.cu
+++ src/cuda_mpi_routines.cu
@@ -16,8 +16,12 @@ int initialize_cuda_mpi(int myid, int nprocs)
   //get the number of cuda devices    
   flag_error = cudaGetDeviceCount(&n_device);
   
-  // Set n_device = 6 for summit testing
-  n_device = 6;
+  #ifdef N_DEVICE_PER_NODE
+  // Override the number of devices per Node, solve gpu errors on summit
+  chprintf( "N CUDA devices per Node: %d \n", N_DEVICE_PER_NODE );
+  n_device = N_DEVICE_PER_NODE;
+  #endif
+  
 
   //check for errors
   if(flag_error!=cudaSuccess)
@@ -36,8 +40,8 @@ int initialize_cuda_mpi(int myid, int nprocs)
   //double check
   cudaGetDevice(&i_device);
 
-  printf("In initialize_cuda_mpi: myid = %d, i_device = %d, n_device = %d\n",myid,i_device,n_device);
-  fflush(stdout);
+  // printf("In initialize_cuda_mpi: myid = %d, i_device = %d, n_device = %d\n",myid,i_device,n_device);
+  // fflush(stdout);
 
   return 0;
     
